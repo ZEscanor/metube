2b02361ce7778d3dfbaf3db2188fcd661fa13fbb
Yinusa <yinusaamusa137@gmail.com>
2022-10-19 18:21:46 -0500

    resp

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -58,7 +58,7 @@ const VideoDetail = () => {
     <Box minHeight= "95vh">
       <Stack direction= {{xs: 'column', md: 'row'}}>
         <Box flex={1}>
-          <Box sx={{width:'100%', position: 'sticky', top: '86px'}}>
+          <Box sx={{width:{xs:'90%', md: "100%"}, position: 'sticky', top: '86px'}}>
             <ReactPlayer url={`https//www.youtube.com/watch?v=${id}`}
            className= 'react-player' controls
           />
